3aef1aaaf848672f5fe94855b9f799914f3457a3
amazingguni <amazingguni@gmail.com>
2025-11-30 21:37:09 +0900

    Remove ShareButton component and its associated styles from the application

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { Gallery } from "./component/gallery"
 import { Information } from "./component/information"
 import { GuestBook } from "./component/guestbook"
 import { LazyDiv } from "./component/lazyDiv"
-import { ShareButton } from "./component/shareButton"
 import { STATIC_ONLY } from "./env"
 
 function App() {
@@ -36,8 +35,6 @@ function App() {
           {/* 오시는길 */}
           <Location />
         </LazyDiv>
-        
-        <ShareButton />
       </div>
     </div>
   )
